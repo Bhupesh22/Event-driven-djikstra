3b33cd8e2bb908346e44850c58128e0bcb725fdc
Bhupesh Patil <bhupeshp286@gmail.com>
2024-09-10 22:32:32 -0400

    Added djikstra solver and refactor

--- src/main/java/com/spring/kafka/eventdrivendjikstra/ConsumerService.java
+++ src/main/java/com/spring/kafka/eventdrivendjikstra/ConsumerService.java
@@ -17,15 +17,14 @@ public class ConsumerService {
     @KafkaListener(topics = "graph_topic", groupId = "group_id")
     public void consume(String message) {
         try {
-            // Deserialize the JSON message to a Graph object
+
             Graph graph = objectMapper.readValue(message, Graph.class);
 
-            // Convert flights list to an int[][] array for the algorithm
+
             int[][] flights = graph.getFlights().stream()
                     .map(l -> l.stream().mapToInt(Integer::intValue).toArray())
                     .toArray(int[][]::new);
 
-            // Call the algorithm to find the cheapest price
             Solution solution = new Solution();
             int cheapestPrice = solution.findCheapestPrice(
                     graph.getN(),
